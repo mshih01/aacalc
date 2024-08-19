6b616a6d18628c06f21f1a707d9a67cbb0774d06
mshih01 <mitchell_shih@yahoo.com>
2024-08-18 17:17:29 -0700

    move external interfaces to the top of the file.

--- test.ts
+++ test.ts
@@ -1,3 +1,149 @@
+
+type UnitIdentifier = "aa" | "inf" | "art" | "arm" | "fig" | "bom" | "sub" | "tra" | "des" | "cru" | "acc" | "bat" | "bat1" | "dbat" | "ic" | "inf_a" | "art_a" | "arm_a" | "internal";
+
+interface UnitSubgroup {
+    unitId : UnitIdentifier,		
+	count : number
+}
+
+interface UnitGroup {
+	units : UnitSubgroup[],
+	ool :   UnitIdentifier[]		// units as above
+	takes : number,			// number of land unto take as attacker
+	aaLast : boolean		// aa last as defender
+}
+
+interface WaveInput {
+	attack : 	UnitGroup,
+	defense : 	UnitGroup,
+	att_submerge : boolean,
+	def_submerge : boolean,
+	att_dest_last : boolean,
+	def_dest_last : boolean,
+	retreat_threshold : number		// retreat if <= number of units remaining.
+}
+
+interface MultiwaveInput {
+	wave_info : WaveInput[];
+	debug	: boolean;
+	prune_threshold : number;
+	report_prune_threshold : number;
+	is_naval : boolean;
+	num_runs	: number;
+}
+
+type Side = "attack" | "defense"
+export type CasualtiesInfo = Record<Side, Record<string, CasualtyInfo>>;
+
+export interface CasualtyInfo {
+  casualties: string
+  survivors: string
+  retreaters: string
+  amount: number
+  ipcLoss: number
+}
+
+interface CalcInfo {
+	survives : number[] 
+	ipcLoss : number[]
+}
+
+interface MultiwaveOutput {
+	attack : CalcInfo;
+	defense : CalcInfo;
+	casualtiesInfo : CasualtiesInfo;
+	takesTerritory : number[];
+	rounds : number[];
+	waves : number;
+}
+
+function multiwaveExternal(
+		input : MultiwaveInput
+		) 
+   : MultiwaveOutput
+{
+	let internal_input : multiwave_input;
+
+	let wavearr : wave_input[] = [];
+	for (let i = 0; i < input.wave_info.length; i++) { 
+		let wave = input.wave_info[i];
+		let [att_unitstr, att_oolstr] = make_unit_group_string(
+			wave.attack.units, wave.attack.ool, wave.attack.takes, false, input.is_naval);
+		let [def_unitstr, def_oolstr] = make_unit_group_string(
+			wave.defense.units, wave.defense.ool, 0, wave.defense.aaLast, input.is_naval);
+		
+		let internal_wave = { attacker : att_unitstr, 
+			  defender : def_unitstr,
+			  def_ool : def_oolstr,
+			  att_submerge : wave.att_submerge,
+			  def_submerge : wave.def_submerge,
+			  att_dest_last : wave.att_dest_last,
+			  def_dest_last : wave.def_dest_last,
+			  retreat_threshold : wave.retreat_threshold };
+		
+		wavearr.push(internal_wave);
+	}
+
+	internal_input = {
+		wave_info : wavearr,
+		debug	: input.debug,
+		prune_threshold : input.prune_threshold,
+		report_prune_threshold : input.report_prune_threshold,
+		is_naval : input.is_naval,
+		num_runs	: input.num_runs
+		}
+
+	let internal_output = multiwave(internal_input);
+    let out : MultiwaveOutput;
+	let rounds : number[] = [];
+	for (let i = 0; i < internal_output.output.length; i++) {
+		rounds.push(-1);
+	}
+	let casualtiesInfo : CasualtiesInfo = { attack : {}, defense : {}};
+	let att : Record<string, CasualtyInfo> = {};
+	let def : Record<string, CasualtyInfo> = {};
+   
+	let lastWave = internal_output.output.length - 1;
+	let lastOutput = internal_output.output[lastWave];
+	let um = new unit_manager();
+	for (let i = 0; i < lastOutput.att_cas.length; i++) {
+		let cas = lastOutput.att_cas[i];
+		let casualty : CasualtyInfo;
+		casualty = { casualties : get_external_unit_str(um, cas.casualty),	
+					survivors :  get_external_unit_str(um, cas.remain),
+					retreaters : "",
+					amount : 	cas.prob,	
+					ipcLoss :    get_cost_from_str(um, cas.casualty)
+					}
+		att[i] = casualty;	
+	}
+	for (let i = 0; i < lastOutput.def_cas.length; i++) {
+		let cas = lastOutput.def_cas[i];
+		let casualty : CasualtyInfo;
+		casualty = { casualties : get_external_unit_str(um, cas.casualty),	
+					survivors :  get_external_unit_str(um, cas.remain),
+					retreaters : "",
+					amount : 	cas.prob,	
+					ipcLoss :    get_cost_from_str(um, cas.casualty)
+					}
+		def[i] = casualty;	
+	}
+	casualtiesInfo["attack"] = att;
+	casualtiesInfo["defense"] = def;
+	
+	out = {
+		attack : internal_output.out.attack,
+		defense : internal_output.out.defense,
+		waves : internal_output.output.length,		
+		takesTerritory : internal_output.out.takesTerritory,
+		rounds : rounds,
+		casualtiesInfo : casualtiesInfo
+	}
+	
+    return out;
+}
+
+
 import * as readline from 'readline';
 
 import { Heap } from 'heap-js';
@@ -3326,154 +3472,9 @@ function run4(argc : number, argv : string[])
 		num_runs : 1
 	}
 		
+	console.log(JSON.stringify(input, null, 4));
 	let output = multiwaveExternal(input);
+	console.log(JSON.stringify(input, null, 4));
 	console.log(JSON.stringify(output, null, 4));
 }
-
-
-
-type UnitIdentifier = "aa" | "inf" | "art" | "arm" | "fig" | "bom" | "sub" | "tra" | "des" | "cru" | "acc" | "bat" | "bat1" | "dbat" | "ic" | "inf_a" | "art_a" | "arm_a" | "internal";
-
-interface UnitSubgroup {
-    unitId : UnitIdentifier,		
-	count : number
-}
-
-interface UnitGroup {
-	units : UnitSubgroup[],
-	ool :   UnitIdentifier[]		// units as above
-	takes : number,			// number of land unto take as attacker
-	aaLast : boolean		// aa last as defender
-}
-
-interface WaveInput {
-	attack : 	UnitGroup,
-	defense : 	UnitGroup,
-	att_submerge : boolean,
-	def_submerge : boolean,
-	att_dest_last : boolean,
-	def_dest_last : boolean,
-	retreat_threshold : number		// retreat if <= number of units remaining.
-}
-
-interface MultiwaveInput {
-	wave_info : WaveInput[];
-	debug	: boolean;
-	prune_threshold : number;
-	report_prune_threshold : number;
-	is_naval : boolean;
-	num_runs	: number;
-}
-
-type Side = "attack" | "defense"
-export type CasualtiesInfo = Record<Side, Record<string, CasualtyInfo>>;
-
-export interface CasualtyInfo {
-  casualties: string
-  survivors: string
-  retreaters: string
-  amount: number
-  ipcLoss: number
-}
-
-interface CalcInfo {
-	survives : number[] 
-	ipcLoss : number[]
-}
-
-interface MultiwaveOutput {
-	attack : CalcInfo;
-	defense : CalcInfo;
-	casualtiesInfo : CasualtiesInfo;
-	takesTerritory : number[];
-	rounds : number[];
-	waves : number;
-}
-
-function multiwaveExternal(
-		input : MultiwaveInput
-		) 
-   : MultiwaveOutput
-{
-	let internal_input : multiwave_input;
-
-	let wavearr : wave_input[] = [];
-	for (let i = 0; i < input.wave_info.length; i++) { 
-		let wave = input.wave_info[i];
-		let [att_unitstr, att_oolstr] = make_unit_group_string(
-			wave.attack.units, wave.attack.ool, wave.attack.takes, false, input.is_naval);
-		let [def_unitstr, def_oolstr] = make_unit_group_string(
-			wave.defense.units, wave.defense.ool, 0, wave.defense.aaLast, input.is_naval);
-		
-		let internal_wave = { attacker : att_unitstr, 
-			  defender : def_unitstr,
-			  def_ool : def_oolstr,
-			  att_submerge : wave.att_submerge,
-			  def_submerge : wave.def_submerge,
-			  att_dest_last : wave.att_dest_last,
-			  def_dest_last : wave.def_dest_last,
-			  retreat_threshold : wave.retreat_threshold };
-		
-		wavearr.push(internal_wave);
-	}
-
-	internal_input = {
-		wave_info : wavearr,
-		debug	: input.debug,
-		prune_threshold : input.prune_threshold,
-		report_prune_threshold : input.report_prune_threshold,
-		is_naval : input.is_naval,
-		num_runs	: input.num_runs
-		}
-
-	let internal_output = multiwave(internal_input);
-    let out : MultiwaveOutput;
-	let rounds : number[] = [];
-	for (let i = 0; i < internal_output.output.length; i++) {
-		rounds.push(-1);
-	}
-	let casualtiesInfo : CasualtiesInfo = { attack : {}, defense : {}};
-	let att : Record<string, CasualtyInfo> = {};
-	let def : Record<string, CasualtyInfo> = {};
-   
-	let lastWave = internal_output.output.length - 1;
-	let lastOutput = internal_output.output[lastWave];
-	let um = new unit_manager();
-	for (let i = 0; i < lastOutput.att_cas.length; i++) {
-		let cas = lastOutput.att_cas[i];
-		let casualty : CasualtyInfo;
-		casualty = { casualties : get_external_unit_str(um, cas.casualty),	
-					survivors :  get_external_unit_str(um, cas.remain),
-					retreaters : "",
-					amount : 	cas.prob,	
-					ipcLoss :    get_cost_from_str(um, cas.casualty)
-					}
-		att[i] = casualty;	
-	}
-	for (let i = 0; i < lastOutput.def_cas.length; i++) {
-		let cas = lastOutput.def_cas[i];
-		let casualty : CasualtyInfo;
-		casualty = { casualties : get_external_unit_str(um, cas.casualty),	
-					survivors :  get_external_unit_str(um, cas.remain),
-					retreaters : "",
-					amount : 	cas.prob,	
-					ipcLoss :    get_cost_from_str(um, cas.casualty)
-					}
-		def[i] = casualty;	
-	}
-	casualtiesInfo["attack"] = att;
-	casualtiesInfo["defense"] = def;
-	
-	out = {
-		attack : internal_output.out.attack,
-		defense : internal_output.out.defense,
-		waves : internal_output.output.length,		
-		takesTerritory : internal_output.out.takesTerritory,
-		rounds : rounds,
-		casualtiesInfo : casualtiesInfo
-	}
-	
-    return out;
-}
-
 
